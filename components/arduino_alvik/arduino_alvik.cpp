c27f2433e6811e9c106fa8182d03be37acd4e74c
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-09 10:15:15 +0100

    Update arduino_alvik.cpp

--- components/arduino_alvik/arduino_alvik.cpp
+++ components/arduino_alvik/arduino_alvik.cpp
@@ -30,17 +30,6 @@ void AlvikComponent::setup() {
 
 void AlvikComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "HunterWifiComponent  :");
-  LOG_PIN("  Pin: ", this->pin_);
-  for (size_t valve_number = 0; valve_number < this->number_of_valves(); valve_number++) {
-    ESP_LOGCONFIG(TAG, "  Valve %u:", valve_number);
-    ESP_LOGCONFIG(TAG, "    Name: %s", this->valve_[valve_number].valve_switch->get_name().c_str());
-    ESP_LOGCONFIG(TAG, "    Zone: %u", this->valve_[valve_number].zone_number);
-    ESP_LOGCONFIG(TAG, "    Max_Duration: %u", this->valve_[valve_number].max_duration);
-    if (this->valve_[valve_number].duration_number_name)
-    {
-      ESP_LOGCONFIG(TAG, "    Duration Number Name: %s", this->valve_[valve_number].duration_number_name->get_name().c_str());
-    }
-  }
 }
   
 
