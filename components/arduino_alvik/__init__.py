627084b8778b88e6a3ad575a96efb1170e4901ef
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-09 09:53:07 +0100

    Create __init__.py

---
+++ components/arduino_alvik/__init__.py
@@ -0,0 +1,42 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import number
+from esphome.components import switch
+from esphome.components import button
+from esphome.components import sensor
+from esphome.components import text_sensor
+
+from esphome.const import (
+    CONF_ID, 
+    CONF_NAME, 
+    CONF_NUMBER, 
+    CONF_ENTITY_CATEGORY, 
+    ENTITY_CATEGORY_CONFIG, 
+    CONF_INITIAL_VALUE,
+    CONF_MAX_VALUE,
+    CONF_MIN_VALUE,
+    CONF_RESTORE_VALUE,
+    CONF_STEP,
+)
+
+MULTI_CONF = False; #can be True in future if I understand the consequences
+AUTO_LOAD = ["number", "switch", "button", "sensor", "text_sensor"]
+CODEOWNERS = ["pipacsba"]
+
+alvik_ns = cg.esphome_ns.namespace("alvik")
+AlvikComponent = alvik_ns.class_("AlvikComponent", cg.Component)
+ArduinoAlvik = alvik_ns.class_(
+    "ArduinoAlvik", cg.Component
+)
+
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(ArduinoAlvik),
+        }
+    )
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
