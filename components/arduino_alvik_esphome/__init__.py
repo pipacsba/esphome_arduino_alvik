baec283e18ed121cd6e1b42a3766600db6d6b3d9
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-10 20:35:21 +0100

    Update __init__.py

--- components/arduino_alvik_esphome/__init__.py
+++ components/arduino_alvik_esphome/__init__.py
@@ -27,9 +27,6 @@ AUTO_LOAD = ["number", "switch", "button", "sensor", "text_sensor"]
 CODEOWNERS = ["pipacsba"]
 
 CONF_ALVIK_ID = "alvik_id"
-CONF_BATTERY_CHARGE_SENSOR = "battery_charge"
-CONF_FW_VERSION_SENSOR = "firmware_version"
-CONF_LIB_VERSION_SENSOR = "library_version"
 
 alvik_ns = cg.esphome_ns.namespace("alvik")
 AlvikComponent = alvik_ns.class_("AlvikComponent", cg.Component)
@@ -37,20 +34,6 @@ AlvikComponent = alvik_ns.class_("AlvikComponent", cg.Component)
 ALVIK_COMPONENT_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_ALVIK_ID): cv.use_id(AlvikComponent),
-    }
-)
-
-ALVIK_COMPONENT_SCHEMA = ALVIK_COMPONENT_SCHEMA.extend(
-    {
-        cv.Optional(CONF_BATTERY_CHARGE_SENSOR): sensor.sensor_schema(
-            unit_of_measurement=UNIT_PERCENT,
-            device_class=DEVICE_CLASS_BATTERY,
-            state_class=STATE_CLASS_MEASUREMENT
-        ),
-        cv.Optional(CONF_FW_VERSION_SENSOR): text_sensor.text_sensor_schema(
-        ),
-        cv.Optional(CONF_LIB_VERSION_SENSOR): text_sensor.text_sensor_schema(
-        ),
     }
 )
 
@@ -65,17 +48,3 @@ CONFIG_SCHEMA = cv.All(
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-
-    alvik_id = await cg.get_variable(config[CONF_ALVIK_ID])
-
-    if battery_charge_sensor_config := config.get(CONF_BATTERY_CHARGE_SENSOR):
-        sens = await sensor.new_sensor(battery_charge_sensor_config)
-        cg.add(mainboard.get_battery(sens))
-
-    if fw_version_config := config.get(CONF_FW_VERSION_SENSOR):
-        tsens = await text_sensor.new_text_sensor(fw_version_config)
-        cg.add(mainboard.get_fw_version(tsens))
-
-    if lib_version_config := config.get(CONF_LIB_VERSION_SENSOR):
-        tsens = await text_sensor.new_text_sensor(lib_version_config)
-        cg.add(mainboard.get_fw_version(tsens))
