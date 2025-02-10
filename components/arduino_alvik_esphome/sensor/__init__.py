1c206a9be19de53f9350cda57806b5e4a8f2c64d
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-10 20:28:51 +0100

    Update __init__.py

--- components/arduino_alvik_esphome/sensor/__init__.py
+++ components/arduino_alvik_esphome/sensor/__init__.py
@@ -1,1 +1,48 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import number
+from esphome.components import switch
+from esphome.components import button
+from esphome.components import sensor
+from esphome.components import text_sensor
+
+from esphome.const import (
+    CONF_ID, 
+    CONF_NAME, 
+    CONF_NUMBER, 
+    CONF_ENTITY_CATEGORY, 
+    ENTITY_CATEGORY_CONFIG, 
+    CONF_INITIAL_VALUE,
+    CONF_MAX_VALUE,
+    CONF_MIN_VALUE,
+    CONF_RESTORE_VALUE,
+    CONF_STEP,
+    DEVICE_CLASS_BATTERY,
+    UNIT_PERCENT,
+    STATE_CLASS_MEASUREMENT,
+)
+
+from .. import (
+    CONF_ALVIK_ID,
+    ALVIK_COMPONENT_SCHEMA,
+)
+
+CONF_BATTERY_CHARGE_SENSOR = "battery_charge"
+
+ALVIK_COMPONENT_SCHEMA = ALVIK_COMPONENT_SCHEMA.extend(
+    {
+        cv.Optional(CONF_BATTERY_CHARGE_SENSOR): sensor.sensor_schema(
+            unit_of_measurement=UNIT_PERCENT,
+            device_class=DEVICE_CLASS_BATTERY,
+            state_class=STATE_CLASS_MEASUREMENT
+        ),
+    }
+)
+
+async def to_code(config):
+    alvik_id = await cg.get_variable(config[CONF_ALVIK_ID])
+
+    if battery_charge_sensor_config := config.get(CONF_BATTERY_CHARGE_SENSOR):
+        sens = await sensor.new_sensor(battery_charge_sensor_config)
+        cg.add(mainboard.get_battery(sens))
 
