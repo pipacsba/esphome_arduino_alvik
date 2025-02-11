363b4de383215b50755d3e735cd98ec30473a65c
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-11 14:24:09 +0100

    Update __init__.py

--- components/arduino_alvik_esphome/switch/__init__.py
+++ components/arduino_alvik_esphome/switch/__init__.py
@@ -25,6 +25,7 @@ from esphome.const import (
 from .. import (
     CONF_ALVIK_ID,
     ALVIK_COMPONENT_SCHEMA,
+    TASK_UPDATE_TYPES,
 )
 
 AlvikSwitch = powerfeather_ns.class_("AlvikSwitch", switch.Switch, cg.Component)
