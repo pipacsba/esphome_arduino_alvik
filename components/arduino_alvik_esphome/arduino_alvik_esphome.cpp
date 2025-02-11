db7ab4fd1b08b91f84ba38581c4156e37509e88f
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-11 19:42:22 +0100

    Update arduino_alvik_esphome.cpp

--- components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
+++ components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
@@ -33,7 +33,7 @@ namespace alvik {
       ESP_LOGCONFIG(TAG, "AlvikComponent  : something is done!");
     }
 
-    void EnableAlvikSwitch::write_state(bool state)
+    void AlvikEnableSwitch::write_state(bool state)
     {
         if (state)
         {
