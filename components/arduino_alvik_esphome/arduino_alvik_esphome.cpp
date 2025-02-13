ea6876133cbd60c2cb1eae2b31fdb4e3f8b1cba0
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-13 12:53:07 +0100

    Update arduino_alvik_esphome.cpp

--- components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
+++ components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
@@ -20,7 +20,7 @@ namespace alvik {
     }
 
     void AlvikComponent::setup() {
-        
+        this->set_cycle(0);
 //          uint8_t battery_regs[] = {0, 0};
 //          if ((this->write(&BATTERY_REGISTER, 1, false) != i2c::ERROR_OK) || !this->read_bytes_raw(battery_regs, 2)) {
 //                ESP_LOGE(TAG, "Unable to i2c battery data");
@@ -30,6 +30,7 @@ namespace alvik {
 //          uint16_t battery_val = encode_uint16(battery_regs[1], battery_regs[0]);
 //          float battery_soc = battery_val * 0.00390625;
 //          ESP_LOGD(TAG, "Read battery data: %d, %0.1f", battery_val, battery_soc);
+           
     }
 
     void AlvikComponent::loop() {
@@ -37,11 +38,18 @@ namespace alvik {
         bool ison = this->pin_->digital_read();
         if (ison & !this->stm32_is_on_)
         {
-            this->set_stm32_state(ison);
+           this->set_stm32_state(ison);
            ESP_LOGCONFIG(TAG, "The STM32 is turned on!");
+           this->set_cycle(0);
         }
         else if (ison & this->stm32_is_on_)
         {
+            this->set_cycle(this->cycle_ + 1);
+            switch this->cycle:
+                case 1:
+                default:
+
+            
             //this is were we can do something with the Alvik
         }
         else if  (!ison & this->stm32_is_on_)
