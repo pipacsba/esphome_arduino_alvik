7aa63344f1859679e98f1daf6faeaf25a4bbc059
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-19 21:58:34 +0100

    Update arduino_alvik_esphome.cpp

--- components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
+++ components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
@@ -324,7 +324,7 @@ namespace alvik {
   //      {
             this->i2c_switch1_pin_->pin_mode(gpio::FLAG_INPUT);
             this->i2c_switch2_pin_->pin_mode(gpio::FLAG_INPUT); 
-            //this->battery_sensor_->bus_->recover_()
+            this->battery_sensor_->setup()
             ESP_LOGD(TAG, "I2C recover initiated");
   //      }
   //      if (this->cycle_ == 50)
