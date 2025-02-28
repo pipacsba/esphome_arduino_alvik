634485ab1d9a59276b18f4ea61c15c16983c5abb
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-02-28 18:09:49 +0100

    Update arduino_alvik_esphome.cpp

--- components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
+++ components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
@@ -134,7 +134,7 @@ namespace alvik {
        {
             this->compass_sensor_->write_byte(M_REG_M, 0x00);
             this->compass_sensor_->write_byte(CRA_REG_M, 0x08);  // 3Hz
-            this->compass_sensor_->write_byte(CRB_REG_M, 0x20);  // 1.3Gauss range
+            //this->compass_sensor_->write_byte(CRB_REG_M, 0x10);  // 1.3Gauss range
             this->compass_sensor_->write_byte(M_REG_M, 0x00);    // continous measuremeent mode
             this->compass_x_min = 55;//51.36; //50.91;
             this->compass_x_max = 55;//61.64; 
