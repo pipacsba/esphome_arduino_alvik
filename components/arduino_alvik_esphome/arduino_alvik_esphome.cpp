c2f6f54e0da49db9804865437b32feb5d6824792
pipacsba <44190358+pipacsba@users.noreply.github.com>
2025-03-05 14:54:26 +0100

    Update arduino_alvik_esphome.cpp

--- components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
+++ components/arduino_alvik_esphome/arduino_alvik_esphome.cpp
@@ -94,7 +94,7 @@ namespace alvik {
         this->turn_degree_number_->publish_state(90);
         this->set_turn_degree(90);
         this->follow_distance_number_->publish_state(150);
-        this->follow_tolerance_number_->publish_state(50);
+        this->follow_tolerance_number_->publish_state(20);
         this->follow_gain_horizontal_number_->publish_state(50);
         this->follow_gain_front_number_->publish_state(10);
         
